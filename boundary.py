corrected normal velocity calculation

boundary.py
@@ -20,14 +20,17 @@ def split(Vt, x, y):
 
     return Veu, Vel, su, sl, istag
 
-def combine(thetau, thetal, Hu, Hl, H1u, H1l, cfu, cfl):
-
+def combine(Veu, su, thetau, Hu, H1u, cfu,
+            Vel, sl, thetal, Hl, H1l, cfl):
+    
+    Ve = np.concatenate((np.flip(Vel[1:]), Veu))
+    s = np.concatenate((np.flip(sl[1:]), su))
     theta = np.concatenate((np.flip(thetal[1:]), thetau))
     H = np.concatenate((np.flip(Hl[1:]), Hu))
     H1 = np.concatenate((np.flip(H1l[1:]), H1u))
     cf = np.concatenate((np.flip(cfl[1:]), cfu))
 
-    return theta, H, H1, cf
+    return Ve, s, theta, H, H1, cf
 
 def solve(Ve, s, Re, Htran, trim):
 

main.py
@@ -7,9 +7,9 @@ M = .02                     # maximum camber [% chord]
 P = .40                     # maximum camber position [% chord]
 t = .12                     # thickness [% chord]
 n = 128                     # half number of panels
-alpha = np.radians(0.)      # angle of attack [rad]
-Re = 5e6                    # Reynolds number
-Htran = 1.35                # transition shape factor (1.3 to 1.4)
+alpha = np.radians(4.3)     # angle of attack [rad]
+Re = 5.4e6                  # Reynolds number
+Htran = 1.4                 # transition shape factor (1.3 to 1.4)
 trim = n//20                # trim turbulent trailing edge
 
 X, Y, x, y, S, beta = panel.airfoil(M, P, t, n)
@@ -21,21 +21,21 @@ Vt, cl1, cp1 = panel.solve(An, At, RHS, alpha, beta, S)
 Veu, Vel, su, sl, istag = boundary.split(Vt, x, y)
 thetau, Hu, H1u, cfu, cdu, itranu = boundary.solve(Veu, su, Re, Htran, trim)
 thetal, Hl, H1l, cfl, cdl, itranl = boundary.solve(Vel, sl, Re, Htran, trim)
-theta, H, H1, cf = boundary.combine(thetau, thetal, Hu, Hl, H1u, H1l, cfu, cfl)
+Ve, s, theta, H, H1, cf = boundary.combine(Veu, su, thetau, Hu, H1u, cfu,
+                                           Vel, sl, thetal, Hl, H1l, cfl)
 
-cft = np.sum(cf*Vt**2*S) # total skin friction
-cdt = cdu + cdl # total profile drag
+cft = np.sum(cf*Ve**2*S)        # total skin friction
+cdt = cdu + cdl                 # total profile drag
+ds = theta*H                    # displacement thickness
+d = theta*(H + H1)              # boundary layer thickness
+Vn = np.gradient(Ve*ds, s)      # normal velocity
 
-print('skin friction coefficient: %.5f (%.1f%%)' % (cft, cft/cdt*100))
-print('profile drag coefficient: %.5f' % cdt)
-utils.plot_cf(theta, H, cf, x, trim)
-
-ds = theta*H # displacement thickness
-d = theta*(H + H1) # boundary layer thickness
-
-Vn = np.gradient(Vt*ds) # normal velocity
 RHS = panel.constants(alpha, beta, Vn)
 _, cl2, cp2 = panel.solve(An, At, RHS, alpha, beta, S)
 
+print('skin friction coefficient: %.5f (%.1f%%)' % (cft, cft/cdt*100))
+print('profile drag coefficient: %.5f' % cdt)
 print('lift coefficient: %.5f, %.5f' % (cl1, cl2))
+
+utils.plot_cf(theta, H, cf, x, trim)
 utils.plot_cp(d, ds, cp1, cp2, istag, itranu, itranl, x, y, beta, trim)

utils.py
@@ -17,11 +17,15 @@ def plot_cp(d, ds, cp1, cp2, istag, itranu, itranl, x, y, beta, trim):
     _, ax = plt.subplots()
     axx = ax.twinx()
 
-    n = len(x)//2
-    ax.plot(x[n:-trim], cp2[n:-trim], label=r'$c_{p2,upper}$', zorder=10)
-    ax.plot(x[trim:n], cp2[trim:n], label=r'$c_{p2,lower}$', zorder=10)
-    ax.plot(x[n:-trim], cp1[n:-trim], label=r'$c_{p1,upper}$')
-    ax.plot(x[trim:n], cp1[trim:n], label=r'$c_{p1,lower}$')
+    # verification
+    data = np.loadtxt("verification.txt", delimiter=",")
+    ax.scatter(data[:, 0], data[:, 1], s=20, label='verification')
+
+    cutoff = 24 # change if required
+    ax.plot(x[cutoff:-cutoff], cp2[cutoff:-cutoff], label='viscid')
+    ax.plot(x[trim:-trim], cp1[trim:-trim], label='inviscid')
+    
+    ax.set_ylim([np.min(cp1) - .3, np.max(cp1) + .3])
     ax.set_xlabel(r'$x$')
     ax.set_ylabel(r'$c_p$')
     ax.legend(loc='upper right')
@@ -39,6 +43,7 @@ def plot_cp(d, ds, cp1, cp2, istag, itranu, itranl, x, y, beta, trim):
     axx.scatter([x[istag + itranu - 1], x[istag - itranl + 1]], \
                 [y[istag + itranu - 1], y[istag - itranl + 1]], \
                 color='red', marker='x', s=20, label='transition', zorder=10)
+    
     axx.set_ylabel(r'$y$')
     axx.legend(loc='lower right')
     axx.set_aspect('equal')